18a5e593d7d1047f77a8ac26a6c26e8931f75dcb
Rhange <rhange0511@gmail.com>
2020-05-22 18:55:08 +0900

    Update JS after code review with lecture
    - change 'using fetch' to 'async / await' notation
    - Use user Array not user Object as global variable, using list is more than useful. using object is so hell...

--- 5. DOM Array Methods Project/src/js/script.js
+++ 5. DOM Array Methods Project/src/js/script.js
@@ -5,7 +5,7 @@ const sort = document.getElementById("sort");
 const total = document.getElementById("total");
 const ul = document.getElementById("lists");
 
-let userObj = {};
+let userList = [];
 
 // formatter function makes number to dollar's format
 const formatter = (wealth) => {
@@ -13,97 +13,90 @@ const formatter = (wealth) => {
   return `$${result}`;
 };
 
-// Show userObj info on web page
+// Show userList info on web page
 const paintList = () => {
   // initialize ul
   ul.innerHTML = "";
-
-  // call user's name and wealth from userObj
-  const nameList = Object.keys(userObj);
-  const wealthList = Object.values(userObj);
-
-  // create <li> tag with name[key] and wealth[value]
-  for (let i = 0; i < nameList.length; i++) {
+  userList.forEach((each) => {
     const li = document.createElement("li");
     const person = document.createElement("span");
     const wealth = document.createElement("span");
 
     person.className = "person";
-    person.innerText = nameList[i];
+    person.innerText = `${each.name}`;
     li.appendChild(person);
 
     wealth.className = "money";
-    wealth.innerText = formatter(wealthList[i]);
+    wealth.innerText = formatter(each.wealth);
     li.appendChild(wealth);
 
     // append <li> to <ul> as child
     ul.appendChild(li);
-  }
+  });
 };
 
 // add random user function with userAPI
-const addUser = () => {
-  // get random user from userAPI with fetch
-  fetch("https://randomuser.me/api/")
-    .then((res) => res.json())
-    .then((data) => {
-      const {
-        results: [
-          {
-            name: { first, last }
-          }
-        ]
-      } = data;
+const addUser = async () => {
+  const res = await fetch("https://randomuser.me/api/");
+  const data = await res.json();
+  const {
+    results: [
+      {
+        name: { first, last }
+      }
+    ]
+  } = data;
 
-      const person = `${first} ${last}`;
-      // create user's wealth with random number
-      const wealth = Math.ceil(Math.random() * 1000000 + 100000);
-      // save user info to object
-      userObj[person] = wealth;
-      // call paintList function which show user's info with <li> tag
-      paintList();
-    });
+  const name = `${first} ${last}`;
+  // create user's wealth with random number
+  const wealth = Math.ceil(Math.random() * 1000000 + 100000);
+  // save user info to object
+  const userObj = {
+    name,
+    wealth
+  };
+  // add userObj to userList
+  userList.push(userObj);
+  // call paintList function which show user's info
+  paintList();
 };
 
-// doubleMoney function makes userObj all value twice
+// doubleMoney function makes userList all value twice
 const doubleMoney = () => {
-  Object.keys(userObj).map((each) => (userObj[each] *= 2));
+  userList = userList.map((each) => {
+    return { ...each, wealth: each.wealth * 2 };
+  });
   paintList();
 };
 
 const replaceUserObj = (nameArr) => {
   const newObj = {};
-  nameArr.forEach((each) => (newObj[each] = userObj[each]));
-  userObj = newObj;
+  nameArr.forEach((each) => (newObj[each] = userList[each]));
+  userList = newObj;
 };
 
 // filterMoney function filters keys which values are more than or equal to 1000000
 const filterMoney = () => {
-  const filteredUsers = Object.keys(userObj).filter(
-    (each) => userObj[each] >= 1000000
-  );
-  replaceUserObj(filteredUsers);
+  userList = userList.filter((each) => each.wealth >= 1000000);
   paintList();
 };
 
 // sortMoney function sorting user list with descending order
 const sortMoney = () => {
-  const sortedUsers = Object.keys(userObj)
-    .sort((a, b) => userObj[a] - userObj[b])
-    .reverse();
-  replaceUserObj(sortedUsers);
+  userList.sort((a, b) => b.wealth - a.wealth);
   paintList();
 };
 
 // sumMoney function sum user's wealth and show the result value
 const sumMoney = () => {
-  const wealthArr = Object.values(userObj);
-  const totalWealth = wealthArr.reduce((a, b) => a + b);
+  const totalWealth = userList.reduce((acc, user) => (acc += user.wealth), 0);
 
   const li = document.createElement("li");
   li.className = "total";
+
   const title = document.createElement("span");
   title.innerText = "Total Wealth:";
+
   const value = document.createElement("span");
   value.innerText = formatter(totalWealth);
   value.className = "total-value";
